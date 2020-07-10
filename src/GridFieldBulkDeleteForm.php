fd28505a41b72bb90450b142814a71d35b2280f0
Ryan DJ <ryan.djoenaidi@wundermanthompson.com>
2020-07-10 15:24:06 +1200

    remove unused queue job and fix js script

--- src/GridFieldBulkDeleteForm.php
+++ src/GridFieldBulkDeleteForm.php
@@ -1,41 +1,47 @@
 <?php
 
-namespace DNADesign\GridFieldBulkDelete;
+namespace Heyday\GridFieldBulkDelete;
 
-use SilverStripe\Security\Member;
-use SilverStripe\Forms\HiddenField;
 use SilverStripe\Forms\GridField\GridField_HTMLProvider;
 use SilverStripe\Forms\GridField\GridField_FormAction;
 use SilverStripe\Forms\GridField\GridField_DataManipulator;
 use SilverStripe\Forms\GridField\GridField_ActionProvider;
 use SilverStripe\Forms\GridField\GridFieldPaginator;
 use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\FormField;
-use SilverStripe\Forms\DropdownField;
-use SilverStripe\Core\Config\Config;
 use SilverStripe\Control\Controller;
-use DNADesign\GridFieldBulkDelete\QueuedBulkDeleteJob;
+use SilverStripe\ORM\SS_List;
 
 /**
-* Adds a dropdown and a button
-* Allowig users to delete records in the gridfield in bulk
-* with the option to delete only records older than 1,2,3 or 6 months.
-*
-* Also, if available, a queued job will be used if too many record need to be deleted.
-* Both this task and queuedjob loop trhough the record and invoke "delete"
-* so any dependant record may be deleted as well.
-*
-*/
+ * Adds a dropdown and a button
+ * Allowig users to delete records in the gridfield in bulk
+ * with the option to delete only records older than 1,2,3 or 6 months.
+ *
+ * Also, if available, a queued job will be used if too many record need to be deleted.
+ * Both this task and queuedjob loop trhough the record and invoke "delete"
+ * so any dependant record may be deleted as well.
+ *
+ */
 class GridFieldBulkDeleteForm implements GridField_HTMLProvider, GridField_ActionProvider
 {
+    /**
+     * @var string
+     */
+    const STATUS_GOOD = 'good';
+
+    /**
+     * @var string
+     */
     protected $targetFragment;
 
+    /**
+     * @var string
+     */
     protected $message;
-    
-    protected $status = 'good';
 
-    // Set to less than 0 to never use queuedjob
-    protected $use_queued_threshold = 50;
+    /**
+     * @var string
+     */
+    protected $status = self::STATUS_GOOD;
 
     public function __construct($targetFragment = 'before', $threshold = null)
     {
@@ -46,63 +52,36 @@ class GridFieldBulkDeleteForm implements GridField_HTMLProvider, GridField_Actio
         }
     }
 
+    /**
+     * Return all HTML fragment of delete form
+     * 
+     * @param GridField $gridField
+     * @return array
+     */
     public function getHTMLFragments($gridField)
     {
         $form = $gridField->getForm();
         $records = $this->getFilteredRecordList($gridField);
         if (!$records->exists()) {
-            // If a message exists, but no record
-            // it means we have deleted them all
-            // so display the message anyway
+            /**
+             * If a message exists, but no record
+             * it means we have deleted them all
+             * so display the message anyway
+             */
             if ($this->message) {
                 $fragment = sprintf('<p class="message %s">%s</p>', $this->status, $this->message);
-                return array(
-                    $this->targetFragment => $fragment
-                );
+                return [$this->targetFragment => $fragment];
             }
             // Otherwise hide the form
-            return array();
+            return [];
         }
 
         $singleton = singleton($gridField->getModelClass());
         if (!$singleton->canDelete()) {
-            return array();
+            return [];
         }
 
-        // Always give the option to delete everything
-        $default_option = 'now';
-        $default_option_label = sprintf('Delete all %s records', $records->Count());
-
-        $options = [$default_option => $default_option_label];
-
-        // Fetch other interval from config
-        $up_to = Config::inst()->get(get_class($this), 'delete_up_to');
-        if ($up_to) {
-            if (!is_array($up_to)) {
-                user_error('Time intervals need to be supplied as an array.', E_USER_NOTICE);
-                exit();
-            }
-
-            foreach ($up_to as $interval => $label) {
-                $date = new \DateTime(); // note: core php class
-                $date->modify('-'.$interval);
-
-                if ($date !== false) {
-                    $toDelete = $records->filter('Created:LessThan', $date->format('Y-m-d 00:00:00'))->Count();
-                    $options[$interval] = sprintf($label, $toDelete);
-                }
-            }
-        }
-        
-        if (count($options) > 1) {
-            $optionsField = DropdownField::create('BulkDeleteUntil', '', $options);
-        } else {
-            $optionsField = HiddenField::create('BulkDeleteUntil', $default_option_label, $default_option);
-        }
-        
-        $optionsField->setForm($gridField->getForm());
-
-        $buttonTitle = (count($options) > 1) ? 'Go' : $default_option_label;
+        $buttonTitle = sprintf('Delete %s record(s)', $records->Count());
 
         $button = new GridField_FormAction(
             $gridField,
@@ -113,32 +92,43 @@ class GridFieldBulkDeleteForm implements GridField_HTMLProvider, GridField_Actio
         );
 
         $button->setForm($gridField->getForm());
-        $button->addExtraClass('bulkdelete_button');
+        $button->addExtraClass('bulkdelete_button'); // For JS purpose
+        $button->addExtraClass('font-icon-trash btn btn-outline-danger'); // For styling purpose
 
         // Set message
         if ($this->message) {
-            if (count($options) > 1) {
-                $form->setMessage($this->message, $this->status);
-            }
+            $form->setMessage($this->message, $this->status);
         }
 
-        $template = '<div><table><tr><td style="vertical-align:top;">%s</td><td style="vertical-align:top;"><div style="margin-left: 7px;">%s</div></td></tr></table></div>';
+        $template = '<div><table><tr><td style="vertical-align:top;"><div style="margin-left: 7px;">%s</div></td></tr></table></div>';
 
-        $fragment = sprintf($template, $optionsField->FieldHolder(), $button->Field());
+        $fragment = sprintf($template, $button->Field());
 
-        return array(
+        return [
             $this->targetFragment => $fragment
-        );
+        ];
     }
 
     /**
      * export is an action button
+     * 
+     * @param GridField
+     * @return array
      */
     public function getActions($gridField)
     {
-        return array('bulkdelete');
+        return ['bulkdelete'];
     }
 
+    /**
+     * export is an action button
+     * 
+     * @param string $actionName
+     * @param mixed $arguments
+     * @param mixed $data
+     * 
+     * @return array
+     */
     public function handleAction(GridField $gridField, $actionName, $arguments, $data)
     {
         if ($actionName == 'bulkdelete') {
@@ -149,56 +139,18 @@ class GridFieldBulkDeleteForm implements GridField_HTMLProvider, GridField_Actio
     /**
      * Handle the export, for both the action button and the URL
      */
-    public function handleBulkDelete($gridField, $request = null)
+    public function handleBulkDelete(GridField $gridField)
     {
-        $controller = $gridField->getForm()->getController();
-        $request = $controller->getRequest();
         $records = $this->getFilteredRecordList($gridField);
-        $parent = $controller->currentPage();
 
-        $until = $request->postVar('BulkDeleteUntil');
-        if (!empty($until) && $until !== 'now') {
-            $date = new \DateTime();
-            $date->modify('-'.$until);
-
-            if ($date !== false) {
-                $records = $records->filter('Created:LessThan', $date->format('Y-m-d 00:00:00'));
-            }
-        }
-
-        // If more than 50 record are to be deleted
-        // Use a QueuedJob
-        if (class_exists('QueuedJobService')
-            && $this->use_queued_threshold >= 0
-            && $records->Count() > $this->use_queued_threshold
-        ) {
-            $from = ($parent && $parent->hasMethod('getTitle')) ? $parent->getTitle() : $parent->Name;
-            $title = sprintf('Delete %s record (%s) from %s', $records->Count(), FormField::name_to_label($records->dataClass()), $from);
-            
-            $job = new QueuedBulkDeleteJob($records, $title, Member::currentUser());
-            singleton('QueuedJobService')->queueJob($job);
-
-            $this->message = sprintf('As more than %s records have to be deleted, a job as been queued in the background. You will get an email when the task is complete.', $this->use_queued_threshold);
-            $this->status = 'warning';
-
-            Controller::curr()->getResponse()->setStatusCode(
-                200,
-                $this->message
-            );
-
-            return;
-        }
-
-        // Otherwise start deleting straight away (may time out)
-        $ids = array();
-        
+        $ids = [];
         foreach ($records as $record) {
-            array_push($ids, $record->ID);
+            $ids[] = $record->ID;
             $record->delete();
         }
 
         $this->message = sprintf('%s records have been successfully deleted.', count($ids));
-        $this->status = 'good';
+        $this->status = self::STATUS_GOOD;
 
         Controller::curr()->getResponse()->setStatusCode(
             200,
@@ -209,10 +161,10 @@ class GridFieldBulkDeleteForm implements GridField_HTMLProvider, GridField_Actio
     }
 
     /**
-    * To get the entire list of records with the potential filters
-    * we need to remove the pagination but apply all other filters
-    */
-    public function getFilteredRecordList($gridfield)
+     * To get the entire list of records with the potential filters
+     * we need to remove the pagination but apply all other filters
+     */
+    public function getFilteredRecordList(GridField $gridfield): SS_List
     {
         $list = $gridfield->getList();
 
