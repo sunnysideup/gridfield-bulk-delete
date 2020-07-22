4732e7f6f151d2a6dc5554b92af54b72b7e070a9
scaventum <scaventum@gmail.com>
2020-07-23 09:33:13 +1200

    Update bulkdelete_confirm.js

--- client/js/bulkdelete_confirm.js
+++ client/js/bulkdelete_confirm.js
@@ -1,17 +1,25 @@
 (function ($) {
+	$.entwine(function ($) {
+		/**
+		 * force a confirm when clearing form submissions
+		 */
 
-	$('.cms-edit-form .bulkdelete_button').click(function (e) {
+		$('.cms-edit-form .bulkdelete_button').click(function (e) {
 
-		var button = $(e.target);
-		var action = $.trim(button.text());
-		var message = 'Are you sure you want to ' + action.toLowerCase() + '?';
+			var button = $(e.target),
+				value = button.parents('.ss-gridfield-buttonrow').find('select[name="BulkDeleteUntil"] option:selected').text();
 
-		if (!confirm(message)) {
-			e.preventDefault();
-			return false;
-		} else {
-			this._super(e);
-		}
+			var action = (value) ? value : $.trim(button.text());
+			var message = 'Are you sure you want to ' + action.toLowerCase() + '?';
+
+			if (!confirm(message)) {
+				e.preventDefault();
+				return false;
+			} else {
+				this._super(e);
+			}
+		});
+
 	});
 
 }(jQuery));
